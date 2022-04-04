7cf8ec9ec25ffef36219294f6205ec486c791e2d
zeynepsl <zeyneprsalman@gmail.com>
2022-04-04 15:56:19 +0300

    dtos added

--- carShowroomManagementSystem/src/main/java/uniProject/carShowroomManagementSystem/entities/dtos/CustomerDto.java
+++ carShowroomManagementSystem/src/main/java/uniProject/carShowroomManagementSystem/entities/dtos/CustomerDto.java
@@ -1,15 +1,18 @@
 package uniProject.carShowroomManagementSystem.entities.dtos;
 
-import java.time.LocalDate;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import uniProject.carShowroomManagementSystem.core.entities.Dto;
+import uniProject.carShowroomManagementSystem.core.entities.User;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 @AllArgsConstructor
 @NoArgsConstructor
-public class CustomerDto implements Dto{
-	private LocalDate birthdate;
+public class CustomerDto extends User implements Dto{
+	private String firstName;
+	private String lastName;
+	private String phoneNUmber;
 }
